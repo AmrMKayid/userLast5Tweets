d17190f65208509d6787ecf2ea10d8e5b962514c
Amr M. Kayid <amrkayid2027@gmail.com>
2017-08-08 15:40:17 +0200

    helpers which include method to get the @username timeline

---
+++ helpers.py
@@ -0,0 +1,37 @@
+import html
+import os
+import socket
+
+from twython import Twython
+from twython import TwythonAuthError, TwythonError, TwythonRateLimitError
+
+def get_user_timeline(screen_name, count=5):
+    """Return list of most recent tweets posted by screen_name."""
+
+    # ensure count is valid
+    if count < 1 or count > 5:
+        raise RuntimeError("invalid count")
+
+    # ensure environment variables are set
+    if not os.environ.get("API_KEY"):
+        raise RuntimeError("API_KEY not set")
+    if not os.environ.get("API_SECRET"):
+        raise RuntimeError("API_SECRET not set")
+
+    # get screen_name's (or @screen_name's) most recent tweets
+    # https://dev.twitter.com/rest/reference/get/users/lookup
+    # https://dev.twitter.com/rest/reference/get/statuses/user_timeline
+    # https://github.com/ryanmcgrath/twython/blob/master/twython/endpoints.py
+    try:
+        twitter = Twython(os.environ.get("API_KEY"), os.environ.get("API_SECRET"))
+        user = twitter.lookup_user(screen_name=screen_name.lstrip("@"))
+        if user[0]["protected"]:
+            return None
+        tweets = twitter.get_user_timeline(screen_name=screen_name, count=count)
+        return [html.unescape(tweet["text"].replace("\n", " ")) for tweet in tweets]
+    except TwythonAuthError:
+        raise RuntimeError("invalid API_KEY and/or API_SECRET") from None
+    except TwythonRateLimitError:
+        raise RuntimeError("you've hit a rate limit") from None
+    except TwythonError:
+        return None
