6ffe5cfd3835881d72aea0f85324f44b2ebcc916
Amr M. Kayid <amrkayid2027@gmail.com>
2017-08-08 15:41:13 +0200

    the app that uses flask framework to build the webpage and render the templates

---
+++ application.py
@@ -0,0 +1,25 @@
+from flask import Flask, redirect, render_template, request, url_for
+
+import helpers
+import os
+import sys
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/search")
+def search():
+
+    # validate screen_name
+    screen_name = request.args.get("screen_name", "")
+    if not screen_name:
+        return redirect(url_for("index"))
+
+    # get screen_name's tweets
+    tweets = helpers.get_user_timeline(screen_name)
+
+    # render results
+    return render_template("search.html", tweets=tweets, screen_name=screen_name)
